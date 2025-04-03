a062e1b66b378455871e902b5b3ac509e2fa8094
CleverNamesTaken <root@localhost.localdomain>
2025-04-02 20:23:54 -0400

    Removed stuff

--- AskInstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt install python3-venv -y
-python3 -m venv /usr/local/bin/venv_ask
-/usr/local/bin/venv_ask/bin/pip3 install pyyaml tabulate
-cp ask.py /usr/local/bin/ask
-ask --help
